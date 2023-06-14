c0e42c3c92ddaeabc9958af20b48214f7ec385fd
Geremia Pompei <geremiapompei@gmail.com>
2023-06-14 18:56:08 +0200

    fix

--- src/gridsearch/gridsearch.py
+++ src/gridsearch/gridsearch.py
@@ -16,6 +16,22 @@ def __fix_seed(seed):
     torch.manual_seed(seed)
 
 
+def __read_record_in_file(file_to_save, strategy_key):
+    if file_to_save is not None:
+        if os.path.exists(file_to_save):
+            with open(file_to_save) as file:
+                results = json.load(file)
+                if strategy_key in results:
+                    return results[strategy_key]
+
+
+def __save_record_in_file(file_to_save, strategy_key, strategy_value):
+    results = __read_record_in_file(file_to_save)
+    results[strategy_key] = strategy_value
+    with open(file_to_save, 'w') as file:
+        json.dump(results, file)
+
+
 def gridsearch(
         strategy_builder,
         benchmark_builder,
@@ -28,10 +44,7 @@ def gridsearch(
         verbose=True,
         seed=0,
         file_to_save=None,
-) -> tuple[
-    dict[float, tuple[any, float]],
-    tuple[float, float, dict[str, any]]
-]:
+) -> dict:
     if isinstance(hyperparams_list, dict):
         hyperparams_list = [
             dict(zip(hyperparams_list.keys(), t))
@@ -49,19 +62,18 @@ def gridsearch(
         validation_size=validation_size
     )
 
-    validation_results = {}
-    
-    if file_to_save is not None:
-        if os.path.exists(file_to_save):
-            with open(file_to_save) as file:
-                validation_results = json.load(file)
-        
+    try_to_read = __read_record_in_file(file_to_save, strategy_builder.__class__)
+    results = dict(
+        validation={},
+        test=None,
+    ) if try_to_read is None else try_to_read
+
     if verbose:
         print('Start of validation...')
     for i, hyperparams in enumerate(hyperparams_list):
         print(f'Hyperparams config number {i + 1}/{len(hyperparams_list)}: {hyperparams}')
 
-        if hyperparams in validation_results:
+        if hyperparams in results['validation']:
             continue
 
         AAA, accuracy = run_strategy(
@@ -77,30 +89,32 @@ def gridsearch(
         if verbose:
             print(f'AAA: {AAA}, accuracy: {accuracy}')
 
-        validation_results[hyperparams] = (AAA, accuracy)
+        results['validation'][hyperparams] = (AAA, accuracy)
+        __save_record_in_file(file_to_save, strategy_builder.__class__, results)
 
-        if file_to_save is not None:
-            with open(file_to_save, 'w') as file:
-                json.dump(validation_results, file)
+    best_hyperparams = max({v[0]: k for k, v in results['validation'].items()}.items())[1]
 
-    best_hyperparams = max({v[0]: k for k, v in validation_results.items()}.items())[1]
-    
     if verbose:
         print(f'Best hyperparams: {best_hyperparams}')
         print('End of validation...')
 
-    AAA, accuracy = run_strategy(
-        strategy_builder=strategy_builder,
-        train_stream=benchmark_test.train_stream,
-        eval_stream=benchmark_test.test_stream,
-        model=model_builder(n_classes).to(device),
-        hyperparams=best_hyperparams,
-        num_workers=num_workers,
-        device=device,
-        verbose=verbose,
-    )
-    test_results = AAA, accuracy, best_hyperparams
+    if results['test'] is None:
+        AAA, accuracy = run_strategy(
+            strategy_builder=strategy_builder,
+            train_stream=benchmark_test.train_stream,
+            eval_stream=benchmark_test.test_stream,
+            model=model_builder(n_classes).to(device),
+            hyperparams=best_hyperparams,
+            num_workers=num_workers,
+            device=device,
+            verbose=verbose,
+        )
+        results['test'] = (AAA, accuracy, best_hyperparams)
+        __save_record_in_file(file_to_save, strategy_builder.__class__, results)
+
+    AAA, accuracy, best_hyperparams = results['test']
+
     if verbose:
         print(f'Test results: Accuracy={accuracy}, AAA={AAA}')
 
-    return validation_results, test_results
+    return results
